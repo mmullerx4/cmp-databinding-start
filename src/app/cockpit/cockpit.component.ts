ae2f5594e009ae3e5c027a82a7c144336411348f
mmullerx4 <mmullerx4@gmail.com>
2024-10-01 22:02:40 -0400

    w03-tutorial

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core'; //these need to be called from Angular
+
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+  // Emit an event when a server is created, passing serverName and serverContent.
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+
+  // Emit an event with an alias "bpCreated"
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  //newServerName = '';
+  //newServerContent = '';
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
+
+  constructor() {}
+
+  ngOnInit() {
+
+  }
+
+  onAddServer(nameInput: HTMLInputElement) {
+    console.log(this.serverContentInput)
+    this.serverCreated.emit({
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
+    }); //emit new event of this type ^ serverCreated
+  }
+
+  onAddBlueprint(nameInput: HTMLInputElement) {
+    this.blueprintCreated.emit({
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
+    }); //emit new event of this type ^ serverCreated
+  }
+
+}
+
+
---
+++ src/app/server-element/server-element.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrl: './server-element.component.css',
+  encapsulation: ViewEncapsulation.Emulated      //just this server-element. component will not show encapsulation identifier.   >ShadowDom
+})
+export class ServerElementComponent {
+  @Input('srvElement') element: {type: string, name: string, content: string}; //srvElement is an alias for element
+
+  constructor() {}
+
+  ngOnInit() {
+
+  }
+
+}
